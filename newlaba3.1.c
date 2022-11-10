8d99a0cccb73ee69d1e29f910c467ca1abcab8ff
DrawEvil <115697677+DrawEvil@users.noreply.github.com>
2022-11-10 12:34:04 +0500

    Create newlaba3.1.c

---
+++ newlaba3.1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  double x,f,h,s;
+  int i=0;
+  x=0;
+  printf("Ввидите шаг h (-1<=h<=2) \n");
+  scanf("%lf" ,&h);
+  s=2.00/h;
+  if(h >= -1.00 && h <= 2.00)
+  {
+   printf("\tx\tf(x)\n");
+   printf("------------------\n");
+   while (i<=s)
+   {
+      x=i*h;
+      if(x>=-1 && x<=1)
+        f = exp(-2.0*sin(x));
+      else
+        f = x*x-(1/(tan(x)));
+
+      printf("%lf\t%lf\n", x ,f);
+      i++;
+   }
+  }
+  else
+  {
+   printf("incorect h");
+
+  }
+  return 0;
+
+
+
+}
