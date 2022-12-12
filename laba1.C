a424caa290bdc0e8e86e0df8e63c8d67ca6a42b7
DrawEvil <115697677+DrawEvil@users.noreply.github.com>
2022-12-12 19:09:03 +0500

    Update laba1.C

--- laba1.C
+++ laba1.C
@@ -1,33 +1,31 @@
-##include <stdio.h>
-
-int min(int sum_a, int sum_b)
-{
-    if (sum_a<sum_b) return sum_a;
-    else return sum_b;
-}
-
-int main()
-{
-    int a, b, sum_a=0, sum_b=0, b1=b;
-    scanf("%d", &a);
-    scanf("%d", &b);
-    float y;
-    while (a)
-    {
-
-        sum_a+=a%10;
+#include <stdio.h>
+double sum(int a, int b){
+  int b1=b;
+ double n=0;
+ double m=0;
+    while (a>0){
+        n=n+a%10;
         a/=10;
-
+        
     }
-
-    while(b)
-    {
-
-        sum_b+=b%10;
+    while(b>0){
+        
+        m=m+b%10;
         b/=10;
-
     }
+    if(n>m)
+        return m/b1;    
+    else
+        return n/b1;
+    
+    
+}     
+int main(){
+int a;
+int b;
+scanf("%d", &a);
+scanf("%d", &b);
+printf("%lf" , sum(a,b));
+return 0;
 
-    printf("%f", (float) min(sum_a, sum_b)/b1);
-    return 0;
 }
