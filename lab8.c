18bbb13c544c307df105c4f2f27494654d6ec518
DrawEvil <115697677+DrawEvil@users.noreply.github.com>
2023-01-16 19:53:06 +0500

    Update lab8.c

--- lab8.c
+++ lab8.c
@@ -1,115 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
-##include <stdio.h>
-#include <termios.h>
-#include <unistd.h>
-#include <ctype.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdio_ext.h>
+int main() {
+    char input[100];
+    int sum = 0;
+    printf("Enter a string: ");
+    scanf("%[^\n]s", input);
 
-int mygetch()
-{
-    struct termios oldt, newt;
-    int c;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    c = getchar();
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    return c;
-}
-
-void wheel(int n, int c, int last_num_print[n]) {
-    for (int i = 0; i < n - 1; i += 1) {
-        last_num_print[i] = last_num_print[i + 1];
-    }
-    last_num_print[n] = c;
-}
-
-int main()
-{
-    int c;
-    int count=0;
-    int i, n = 4, end_c, print = 1;
-    int last_num[n], last_num_print[n];
-    for (i = 0; i < n; i += 1) {
-        last_num[i] = 0;
-        last_num_print[i] = 0;
+    char [club38800631|*token] = strtok(input, " ");
+    while (token != NULL) {
+        if (isdigit([club38800631|*token])) {
+            sum += atoi(token);
+        }
+        token = strtok(NULL, " ");
     }
 
-    int end_num[] = {27, 91, 53, 126};
-
-    while (true) {
-        c = mygetch();
-        for (i = 0; i < n - 1; i += 1) {
-            last_num[i] = last_num[i + 1];
-            last_num_print[i] = last_num_print[i + 1];
-        }
-        last_num[n-1] = c;
-        last_num_print[n-1] = c;
-        end_c = 0;
-        for (i = 0; i < n; i += 1) {
-            if (last_num[i] == end_num[i]) {
-                end_c += 1;
-            }
-        }
-        if (n == end_c) {
-            break;
-        }
-        print = 1;
-        for (i = 0; i < n; i += 1) {
-            if (last_num_print[i] == 27) {
-                if ((last_num_print[i] == 27 && last_num_print[i+1] == 79) || (last_num_print[i] == 91 && last_num_print[i+1] == 49)){
-                    print = 0;
-                    wheel(n, c, last_num_print);
-                    wheel(n, c, last_num_print);
-                    __fpurge(stdin);
-                    break;
-                }  
-                print = 0;
-             }
-        }
-        if (print == 1) {
-            if(c!='~'){
-                printf("%c", c);
-            }
-            if(isalpha(c)!=0){
-                count++;
-            }
-        }
-    }
-    printf("\n");
-    printf("count = %d", count);
-    return void getch_to_tilda()
-{
-    int c;
-    do
-    {
-        c = mygetch();
-    } while (c != '~');
+    printf("Sum of integers in the string: %d\n", sum);
+    return 0;
 }
-
-void skip_functional_key()
-{
-    int symType = mygetch();
-    switch (symType)
-    {
-        case '[':
-            int termType = mygetch();
-            if (termType == '1')
-            {
-                int secondChar = mygetch();
-                if (isdigit(secondChar))
-                    getch_to_tilda();
-            }
-            else if (isdigit(termType))
-                getch_to_tilda();
-            break;
-
-        case 'O':
-            mygetch();
-            break;
-    }
-}
-
