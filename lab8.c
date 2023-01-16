ce498f0fec77837ba969d8f46e3e3ba081f6d2f2
DrawEvil <115697677+DrawEvil@users.noreply.github.com>
2023-01-16 19:56:29 +0500

    Update lab8.c

--- lab8.c
+++ lab8.c
@@ -1,20 +1,68 @@
-#include <stdio.h>
-#include <stdlib.h>
+void getch_tilda()
+{
+ int c;
+ do
+ {
+ c = mygetch();
+ }
+ while (c != '~');
+}
 
-int main() {
-    char input[100];
-    int sum = 0;
-    printf("Enter a string: ");
-    scanf("%[^\n]s", input);
+int process_key()
+{
+ int start_char = mygetch();
+ switch (start_char)
+ {
+ case '[':
+ {
+ int first_char = mygetch();
+ if (first_char == '1')
+ {
+ int second_char = mygetch();
+ if (second_char == '9') // Esc[19~
+ return 1;
+ else if (isdigit(second_char))
+ getch_tilda();
+ }
+ else if (isdigit(first_char))
+ getch_tilda();
+ }
+ break;
 
-    char [club38800631|*token] = strtok(input, " ");
-    while (token != NULL) {
-        if (isdigit([club38800631|*token])) {
-            sum += atoi(token);
-        }
-        token = strtok(NULL, " ");
-    }
+ case 'O':
+ {
+ char c = mygetch();
+ if (isdigit(c))
+ mygetch();
+ }
+ break;
+ }
+ 
+ return 0;
+}
 
-    printf("Sum of integers in the string: %d\n", sum);
-    return 0;
+int main()
+{
+ while (1)
+ {
+ char c;
+ c = mygetch();
+ if (iscntrl(c))
+ {
+ if (c == 27)
+ {
+ int result = process_key();
+ if (result == 1)
+ exit(EXIT_SUCCESS);
+ }
+ }
+ else
+ {
+ if (islower(c))
+ c = toupper(c);
+ printf("%c", c);
+ }
+ }
+ printf("\n");
+ return 0;
 }
